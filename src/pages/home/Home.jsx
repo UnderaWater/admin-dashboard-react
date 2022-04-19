bf2d1ecdbe0c8a16887bfd505117070e28f01d37
Kiryl Yermalovich <ts.underawater@gmail.com>
2022-04-19 19:17:28 +0300

    add: added router and pages components

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './home.scss';
+
+const Home = () => {
+    return (
+        <div className='app__home'>
+            <h1 className='app__home-title'>React admin dashboard</h1>
+        </div>
+    )
+};
+
+export default Home;
